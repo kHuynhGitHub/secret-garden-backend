fa892ab3b45a1e105407f29feb29da2645fc035f
dut-a <d.dut@live.com>
2020-01-21 17:09:10 -0600

    Adds new controller aactions

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,10 +2,14 @@ class BooksController < ApplicationController
   
   def index
     books = Book.all
-
     render json: books, :include => {
       :users => { :only => :username }
       }, :except => [:created_at, :updated_at] 
+  end
+
+  def show
+    book = Book.find_by(id: params[:id])
+    render json: book
   end
 
 end
