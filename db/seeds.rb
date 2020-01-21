b8a6ac5101eead23611e0cc8444556bcee080ed3
khuynhgithub <huynh.h.kimberly@gmail.com>
2020-01-21 11:16:56 -0600

    seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+50.times do
+  Books.create(
+    title: Faker::Book.title,
+    descripton: Faker::TvShows::DrWho.quote,
+    cover_image: Faker::LoremFlickr.image(size: "50x60", search_terms: ['books']),
+    author: Faker::Book.author,
+    publisher: Faker::Book.publisher,
+    page_count: Faker::Number.within(range: 100..1000),
+    genre: Faker::Book.genre
+  )
+end
+
+50.times do
+  Author.create(
+    name: Faker::Book.author
+  )
+end
+
+50.times do
+  Publisher.create(
+    name: Faker::Book.publisher
+  )
+end
