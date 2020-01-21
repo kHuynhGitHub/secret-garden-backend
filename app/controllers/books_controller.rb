ef2ae45cb5cc230899b6043c879d4ae998925a13
hfarnsworth <havilahfarnsworth@gmail.com>
2020-01-21 16:52:37 -0600

    fixed controller to not include bookcases

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,9 +4,8 @@ class BooksController < ApplicationController
     books = Book.all
 
     render json: books, :include => {
-      :users => { :only => :username },
-      :bookcases => { :except => [:created_at, :updated_at] }
-      }, :except => [:created_at, :updated_at]
+      :users => { :only => :username }
+      }, :except => [:created_at, :updated_at] 
   end
 
 end
