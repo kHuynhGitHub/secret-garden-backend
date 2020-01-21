4084c34634d54b4da9e9a46f35afb59b4ab9002d
Kymmi <huynh.h.kimberly@gmail.com>
2020-01-21 14:49:56 -0600

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -1,16 +1,31 @@
-
-
-ActiveRecord::Schema.define(version: 2020_01_21_203052) do
-
-  # These are extensions that must be enabled in order to support this database.
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+ActiveRecord::Schema.define(version: 2020_01_21_171521) do
+  # These are extensions that must be enabled in order to support this database
+  # some comment
   enable_extension "plpgsql"
-
   create_table "authors", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
-
+  create_table "bookcases", force: :cascade do |t|
+    t.bigint "book_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["book_id"], name: "index_bookcases_on_book_id"
+    t.index ["user_id"], name: "index_bookcases_on_user_id"
+  end
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "cover_image"
@@ -20,30 +35,17 @@ ActiveRecord::Schema.define(version: 2020_01_21_203052) do
     t.string "genre"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.text "description"
   end
-
-  create_table "books_users", force: :cascade do |t|
-    t.bigint "books_id", null: false
-    t.bigint "users_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["books_id"], name: "index_books_users_on_books_id"
-    t.index ["users_id"], name: "index_books_users_on_users_id"
-  end
-
   create_table "publishers", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
-
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
-
-  add_foreign_key "books_users", "books", column: "books_id"
-  add_foreign_key "books_users", "users", column: "users_id"
+  add_foreign_key "bookcases", "books"
+  add_foreign_key "bookcases", "users"
 end
