1d339e8d148048013a86c8e420e483aac863faf5
dut-a <d.dut@live.com>
2020-01-21 16:40:23 -0600

    Adds Books controller and some routes

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,12 @@
 class BooksController < ApplicationController
+  
+  def index
+    books = Book.all
+
+    render json: books, :include => {
+      :users => { :only => :username },
+      :bookcases => { :except => [:created_at, :updated_at] }
+      }, :except => [:created_at, :updated_at]
+  end
+
 end
