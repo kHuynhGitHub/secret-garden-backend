7ae5954fb69d3e8a17a99660a16044f76e15325f
dut-a <d.dut@live.com>
2020-01-22 16:11:34 -0600

    Completes users 'CRUD' controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,14 +2,96 @@ class UsersController < ApplicationController
   
   def index
     users = User.all
-    render(json: users, :include => {
-      :books => { :except => [:created_at, :updated_at] }
-    }, :except => [:created_at, :updated_at] )
+    if (users.length > 1)
+      final_json = { json: users, :include => {
+            :books => { :except => [:created_at, :updated_at] }
+          },
+          :except => [:created_at, :updated_at]
+      }
+    else
+      final_json = { json: {"status": 204, "message": "No users available."} }
+    end
+    render(final_json)
   end
 
   def show
-    user = User.find_by(id: params[:id])
-    render json: user
+    user = find_user
+    if user
+      final_json = { json: user, :include => {
+        :books => { :except => [:created_at, :updated_at] }
+      }, :except => [:created_at, :updated_at] }
+    else
+      final_json = { json: get_404_error_msg }
+    end
+    render(final_json)
   end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      final_json = { json: {
+          "status": 201,
+          "message": "User created successfully",
+          "data": user
+        }
+      }
+    else
+      final_json = { json: {
+        "status": 422,
+        "errors": user.errors,
+        "error_codes": user.errors.keys.map { |f| "#{f.upcase}_ERROR" }
+      } }
+    end
+    render(final_json)
+  end
+
+  def update
+    user = find_user
+    if user
+      # User found, proceed with other operations on it...
+      user.update(user_params)
+      if user.valid?
+        final_json = { json: {
+            "status": 204,
+            "message": "User updated successfully",
+            "data": user
+          }
+        }
+      else
+        final_json = { json: { "status": 400, "message": "Bad request" } }
+      end
+    else
+      final_json = { json: get_404_error_msg }
+    end
+    render(final_json)
+  end
+
+  def destroy
+    user = find_user
+    if user
+      # User found, proceed with other operations on it...
+      if user.destroy
+        final_json = { json: { "status": 204, "message": "User '#{user.username}', successfully deleted." } }
+      else
+        final_json = { json: get_404_error_msg("Unable to delete user '#{user.username}'.") }
+      end
+    else
+      final_json = { json: get_404_error_msg }
+    end
+    render(final_json)
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:username)
+    end
+
+    def find_user
+      User.find_by(id: params[:id])
+    end
+
+    def get_404_error_msg(msgTxt = "User not found.")
+      return { "status": 404, "message": msgTxt };
+    end
 
 end
