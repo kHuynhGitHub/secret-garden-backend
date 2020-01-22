e24989b42b2a31231920b98db25468f6f9ad91aa
hfarnsworth <havilahfarnsworth@gmail.com>
2020-01-22 09:31:34 -0600

    images seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,25 @@
 require 'faker'
 
+Bookcase.destroy_all
 Book.destroy_all
 User.destroy_all
-Bookcase.destroy_all
+
+imgs = [
+  "assets/images/25a5ea0b96303756c3f3204b9feeaed8--programming-humor-stack-overflow.jpg",
+  "assets/images/0354ce58a7a4308edcc46dd9238e12d7--programming-humor-software-development.jpg",
+  "assets/images/136863.jpg",
+  "assets/images/1787205.jpg",
+  "assets/images/Cf7eHZ1W4AEeZJA.jpg",
+  "assets/images/Cg0x8vnXEAEB2Le.jpg",
+  "assets/images/lrg.jpg",
+  "assets/images/lrg2.jpg"
+]
 
 50.times do
   Book.create(
     title: Faker::Book.title,
     description: Faker::TvShows::DrWho.quote,
-    cover_image: Faker::LoremFlickr.image(size: "50x60", search_terms: ['books']),
+    cover_image: imgs.sample,
     author: Faker::Book.author,
     publisher: Faker::Book.publisher,
     page_count: Faker::Number.within(range: 100..1000),
