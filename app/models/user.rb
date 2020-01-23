588cb97cb4b329e2e49b394fba13713af3371f72
dut-a <d.dut@live.com>
2020-01-23 10:57:06 -0600

    Adds bookcases sorting (ascending)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
-  has_many :bookcases
+  # has_many :bookcases, lambda { order(:book_id => :asc) } # This also works; note the word 'lambda'.
+  has_many :bookcases, -> { order(:book_id => :asc) }
   has_many :books, through: :bookcases
 end
